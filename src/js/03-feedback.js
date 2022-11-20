f37d37f3120f3acf770278c567fb815d111f575c
pickfix <107954584+pickfix@users.noreply.github.com>
2022-11-20 14:43:33 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+  button: document.querySelector('button')
+
+}
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const formDate = {};
+
+texteriaMessage(formDate);
+refs.form.addEventListener('submit', onButtonClick);
+refs.form.addEventListener('input', onInputClick);
+refs.email.addEventListener('input', throttle(onEmail, 500));
+refs.message.addEventListener('input', throttle(onMessage, 500));
+
+function onButtonClick(even) {
+even.preventDefault();
+
+if (refs.email.value === "" || refs.message.value  === "") {
+  alert('All fields must be filled!');
+} else {
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  localStorage.removeItem('Email');
+  even.currentTarget.reset();
+  console.log(formDate);
+};
+
+
+};
+function onInputClick(even) {
+formDate[even.target.name] = e.target.value;
+
+}
+
+function onEmail(even) {
+const email = JSON.stringify(formDate[even.target.name]);
+localStorage.setItem('Email', email);
+
+};
+
+function onMessage(even) {
+  const message = JSON.stringify(formDate[even.target.name]);
+  localStorage.setItem(LOCALSTORAGE_KEY, message);
+ 
+};
+
+function texteriaMessage() {
+  
+ const saveMessage = localStorage.getItem(LOCALSTORAGE_KEY);
+ const saveEmail = localStorage.getItem('Email');
+
+ if (saveMessage) {
+ refs.message.value = JSON.parse(saveMessage);
+ formDate.message = refs.message.value;
+ }
+
+ if (saveEmail) {
+refs.email.value = JSON.parse(saveEmail);
+formDate.email = refs.email.value;
+ }
+ 
+};
